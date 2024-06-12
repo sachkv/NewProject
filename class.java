adca9247e969c9a7a1d79fb8bef59cc5ff5a71b8
sachkv <89726441+sachkv@users.noreply.github.com>
2024-06-12 10:34:07 +0530

    Update class.java

--- class.java
+++ class.java
@@ -1,5 +1,7 @@
-public class Main{
-	public static void main(String[] args){
-		System.out.println("Hello! World");
-	}
+import java.util.*;
+public class GFG {
+    public static void main(String[] args)
+    {
+        System.out.println("GeeksforGeeks!");
+    }
 }
